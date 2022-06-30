dfefcfb5e132382258c4f5137a39b6296526c1d6
Ferhan Abacı <84642053+ferhanabaci@users.noreply.github.com>
2022-06-30 21:43:17 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsin..!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Resim Seçiniz..!");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen minimum 50 karakterlik açıklama bilgisi giriniz.");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("lütfen Açıklamayı kısaltın..");
+
+
+        }
+    }
+}
